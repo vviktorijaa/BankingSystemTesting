c1b25a57278a426eae3b75a0b5fd6e88814834ed
Viktorija Vrzhovska <viktorija-vrzovska@hotmail.com>
2021-10-31 00:10:57 +0200

    finished ISP

--- src/main/java/ISP/BankAccountISP.java
+++ src/main/java/ISP/BankAccountISP.java
@@ -4,38 +4,83 @@ import Bank.BankAccount;
 import Exceptions.MaxBalance;
 import Exceptions.MaxWithdraw;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 public class BankAccountISP {
 
     @Test
-    public void testDeposit(){  //amount = 0
-        BankAccount ba = new BankAccount("CurrentAccount", 7000, 100);
-        assertEquals(7000, ba.deposit(0), 0);
+    public void test1(){    //setName, getName
+        BankAccount ba = new BankAccount();
+        ba.setName("BankAccount");
+        ba.setBalance(5000);
+        ba.setMinBalance(100);
+        assertEquals("BankAccount", ba.getName());
     }
 
     @Test
-    public void testDeposit2(){  //amount > 0
-        BankAccount ba = new BankAccount("CurrentAccount", 10000, 1000);
-        assertEquals(11000, ba.deposit(1000), 0);
+    public void test2(){    //setBalance, getBalance
+        BankAccount ba = new BankAccount();
+        ba.setName("Account");
+        ba.setBalance(7500);
+        ba.setMinBalance(0);
+        assertTrue(ba.getBalance() == 7500);
     }
 
     @Test
-    public void testDeposit3(){  //amount < 0
-        BankAccount ba = new BankAccount("CurrentAccount", 7000, 100);
-        assertEquals(6000, ba.deposit(-1000), 0);
+    public void test3(){    //setBalance, getBalance
+        BankAccount ba = new BankAccount();
+        ba.setName("Account");
+        ba.setBalance(7500);
+        ba.setMinBalance(0);
+        assertTrue(ba.getBalance() == 7500);
     }
 
     @Test
-    public void testWithdraw() throws MaxBalance, MaxWithdraw { //amount^3 <= 0
-        BankAccount ba = new BankAccount("SavingsAccount", 7000, 2000);
-        assertTrue(ba.withdraw(Math.pow(-1, 3)) > 7000);
+    public void test4(){    //setMinBalance, getMinBalance
+        BankAccount ba = new BankAccount();
+        ba.setName("Account");
+        ba.setBalance(100);
+        ba.setMinBalance(100);
+        assertTrue(ba.getMinBalance() == 100);
     }
 
     @Test
-    public void testWithdraw2() throws MaxBalance, MaxWithdraw { //amount^3 > 0
-        BankAccount ba = new BankAccount("SavingsAccount", 2500, 2000);
-        assertEquals(2492, ba.withdraw(Math.pow(2, 3)), 0);
+    public void test6(){    //deposit amt>0
+        BankAccount ba = new BankAccount("BankAccount", 500, 100);
+        assertEquals(1000, ba.deposit(500), 0);
+    }
+
+    @Test
+    public void test7(){    //deposit amt<=0
+        BankAccount ba = new BankAccount("BankAccount", 2000, 50);
+        assertEquals(2000, ba.deposit(0), 0);
+    }
+
+    @Test
+    public void test8() throws MaxBalance, MaxWithdraw {    //withdraw amt>0
+        BankAccount ba = new BankAccount("BankAccount", 2500, 50);
+        assertEquals(2300, ba.withdraw(200), 0);
+    }
+
+    @Test
+    public void test9() throws MaxBalance, MaxWithdraw {    //withdraw amt<=0
+        BankAccount ba = new BankAccount("BankAccount", 4000, 100);
+        assertTrue(ba.withdraw(0) == 4000);
+    }
+
+    @Test
+    public void test10() {    //toString printed
+        BankAccount ba = new BankAccount("BankAccount", 4000, 100);
+        String expected = "Name: "+ba.getName()+", Id: "+ba.getAccNumber()+", Balance: "+ba.getBalance()+", Type: "+ba.getClass();
+        assertEquals(expected, ba.toString());
+    }
+
+    @Test
+    public void test11() {    //toString not printed
+        BankAccount bankAccount = null;
+        assertThrows(NullPointerException.class, () -> {
+            bankAccount.toString();
+        });
     }
 }
---
+++ src/main/java/ISP/SavingsAccountISP.java
@@ -0,0 +1,35 @@
+package ISP;
+
+import Bank.SavingsAccount;
+import Exceptions.MaxBalance;
+import Exceptions.MaxWithdraw;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SavingsAccountISP {
+
+    @Test
+    public void test1(){ //NetBalance is <= 0
+        SavingsAccount sa = new SavingsAccount("SavingsAccount", 0, 1000);
+        assertEquals(0, sa.getNetBalance(), 0);
+    }
+
+    @Test
+    public void test2(){ //NetBalance is > 0
+        SavingsAccount sa = new SavingsAccount("SavingsAccount", 2000, 10000);
+        assertTrue(sa.getNetBalance() > 0);
+    }
+
+    @Test
+    public void test3() throws MaxBalance, MaxWithdraw { //amount is > 0
+        SavingsAccount sa = new SavingsAccount("SavingsAccount", 5000, 5000);
+        assertEquals(500, sa.withdraw(500), 0);
+    }
+
+    @Test
+    public void test4() throws MaxBalance, MaxWithdraw { //amount is <= 0
+        SavingsAccount sa = new SavingsAccount("SavingsAccount", 2000, 500);
+        assertTrue(sa.withdraw(0) == 0);
+    }
+}
